4799411055cac7c4233d9bf43ad6e0df726ac6c4
codehashira28 <123044813+codehashira28@users.noreply.github.com>
2023-09-11 00:32:58 -0500

    added logic to switch between pages when corresponding navigation link is clicked

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
 import { useState } from 'react'
 import Nav from './components/Nav'
+import About from './components/About'
+import Contact from './components/Contact'
+import Portfolio from './components/Portfolio'
+import Resume from './components/Resume'
+
 
 function App() {
-
+  const [currentPage, setPage] = useState('About');
   return (
     <>
-      <Nav />
+      <Nav setPage={ setPage }/>
+      {currentPage === 'About' && <About />}
+      {currentPage === 'Contact' && <Contact />}
+      {currentPage === 'Portfolio' && <Portfolio />}
+      {currentPage === 'Resume' && <Resume />}
     </>
   )
 }
--- src/components/Nav/index.jsx
+++ src/components/Nav/index.jsx
@@ -1,15 +1,20 @@
 import './styles.css'
 
-function Nav() {
+function Nav({ setPage }) {
+    const changePage = (e, page) => {
+        e.preventDefault();
+        setPage(page);
+    }
+
     return (
     <>
         <header className="header-scheme d-flex justify-content-between align-items-center p-5">
             <h1 className="w-50">Joshua Willams</h1>
             <nav className="d-flex justify-content-between w-50">
-                <a className="nav-links">About Me</a>
-                <a className="nav-links">Portfolio</a>
-                <a className="nav-links">Contact</a>
-                <a className="nav-links">Resume</a>
+                <a className="nav-links" onClick={(e) => changePage(e, 'About')}>About Me</a>
+                <a className="nav-links" onClick={(e) => changePage(e, 'Portfolio')}>Portfolio</a>
+                <a className="nav-links" onClick={(e) => changePage(e, 'Contact')}>Contact</a>
+                <a className="nav-links" onClick={(e) => changePage(e, 'Resume')}>Resume</a>
             </nav>
         </header>
     </>
