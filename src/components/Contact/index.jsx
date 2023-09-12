9866ff38829526f8b0906a2207d40a906ca092b9
codehashira28 <123044813+codehashira28@users.noreply.github.com>
2023-09-12 11:51:27 -0500

    changed label for attributes to htmlFor

--- src/components/Contact/index.jsx
+++ src/components/Contact/index.jsx
@@ -5,11 +5,11 @@ function Contact() {
         <div className="p-5" id="contact">
             <h2>Contact</h2>
             <form className='d-flex flex-column gap-2'>
-                <label for="name">Name:</label>
+                <label htmlFor="name">Name:</label>
                 <input required type="text" id="name"/>
-                <label for="email">Email:</label>
+                <label htmlFor="email">Email:</label>
                 <input id="email" required type="email" />
-                <label for="message">Message:</label>
+                <label htmlFor="message">Message:</label>
                 <textarea required rows="6" id="message"></textarea>
                 <button id="submit" className="mt-3 px-2 align-self-start" type="submit">Submit</button>
             </form>
