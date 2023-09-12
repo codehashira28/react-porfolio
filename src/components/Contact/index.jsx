581fd604804ab5e1ac000d062beb95c7181cdf6e
codehashira28 <123044813+codehashira28@users.noreply.github.com>
2023-09-12 11:46:11 -0500

    added code for contact section

--- src/components/Contact/index.jsx
+++ src/components/Contact/index.jsx
@@ -1,6 +1,20 @@
+import './styles.css';
+
 function Contact() {
     return (
-        <h2>Contact</h2>
+        <div className="p-5" id="contact">
+            <h2>Contact</h2>
+            <form className='d-flex flex-column gap-2'>
+                <label for="name">Name:</label>
+                <input required type="text" id="name"/>
+                <label for="email">Email:</label>
+                <input id="email" required type="email" />
+                <label for="message">Message:</label>
+                <textarea required rows="6" id="message"></textarea>
+                <button id="submit" className="mt-3 px-2 align-self-start" type="submit">Submit</button>
+            </form>
+            
+        </div>
     )
 }
 
