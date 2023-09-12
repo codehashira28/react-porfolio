e01b1e383c86e161f0399a85dfe90ea257f9510a
codehashira28 <123044813+codehashira28@users.noreply.github.com>
2023-09-12 15:49:18 -0500

    fleshed out portfolio section

--- src/components/Portfolio/index.jsx
+++ src/components/Portfolio/index.jsx
@@ -1,6 +1,42 @@
+import './styles.css'
+import granulargarden from '../../assets/Granular-Garden-Home.png'
+import pokeinfogenerator from '../../assets/Pokemon-Info-Generator.png'
+import weatherdashboard from '../../assets/Weather-Dashboard.png'
+import techblog from '../../assets/Tech-Blog.png'
+import notetaker from '../../assets/Note-Taker.png'
+import jate from '../../assets/J-A-T-E.png'
+
 function Portfolio() {
     return (
-        <h2>Portfolio</h2>
+        <div id="portfolio" className="p-5">
+            <h2>Portfolio</h2>
+            <div className="mx-3 portfolio-container w-100 d-flex flex-wrap gap-3">
+                <div className="project">
+                    <img className="project-img" src={granulargarden} />
+                    <a className="project-link" href="https://frozen-tor-00446-d883d20dd32e.herokuapp.com/">Granular Garden</a>
+                </div>
+                <div className="project">
+                    <img className="project-img" src={pokeinfogenerator} />
+                    <a className="project-link" href="https://codehashira28.github.io/poke-info-generator/">Poke Info Generator</a>
+                </div>
+                <div className="project">
+                    <img className="project-img" src={weatherdashboard} />
+                    <a className="project-link" href="https://codehashira28.github.io/weather-dashboard/">Weather Dashboard</a>
+                </div>
+                <div className="project">
+                    <img className="project-img" src={techblog} />
+                    <a className="project-link" href="https://tech-blog-app-20230806-f48fb5f8b41d.herokuapp.com/">Tech Blog</a>
+                </div>
+                <div className="project">
+                    <img className="project-img" src={notetaker} />
+                    <a className="project-link" href="https://pacific-river-80544-d245515ec8bd.herokuapp.com/">Note Taker</a>
+                </div>
+                <div className="project">
+                    <img className="project-img" src={jate} />
+                    <a className="project-link" href="https://morning-refuge-10692-76df8b5397e0.herokuapp.com/">Text Editor</a>
+                </div>
+            </div>
+        </div>
     )
 }
 
