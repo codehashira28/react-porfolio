94a02be34102c5120e69432c5d5b3f7e27143900
codehashira28 <123044813+codehashira28@users.noreply.github.com>
2023-09-13 23:59:56 -0500

    refactored code for portfolio section to make one project component with props to create multiple project cards

--- src/components/Portfolio/index.jsx
+++ src/components/Portfolio/index.jsx
@@ -7,53 +7,29 @@ import notetaker from '../../assets/Note-Taker.png'
 import jate from '../../assets/J-A-T-E.png'
 import github from '../../assets/github-logo.png'
 
+function Project(props) {
+    return (
+        <div className="project">
+            <img className="project-img" src={props.image} />
+            <a className="project-link" href={props.appurl}>{props.projectTitle}</a>
+            <a href={props.githuburl}>
+                <img className="github-link" src={github} />
+            </a>
+        </div>
+    )
+}
+
 function Portfolio() {
     return (
         <div id="portfolio" className="p-5">
             <h2>Portfolio</h2>
             <div className="mx-3 portfolio-container w-100 d-flex flex-wrap gap-3">
-                <div className="project">
-                    <img className="project-img" src={granulargarden} />
-                    <a className="project-link" href="https://frozen-tor-00446-d883d20dd32e.herokuapp.com/">Granular Garden</a>
-                    <a href="https://github.com/codehashira28/granular-garden">
-                        <img className="github-link" src={github} />
-                    </a>
-                </div>
-                <div className="project">
-                    <img className="project-img" src={pokeinfogenerator} />
-                    <a className="project-link" href="https://codehashira28.github.io/poke-info-generator/">Poke Info Generator</a>
-                    <a href="https://github.com/codehashira28/poke-info-generator">
-                        <img className="github-link" src={github} />
-                    </a>
-                </div>
-                <div className="project">
-                    <img className="project-img" src={weatherdashboard} />
-                    <a className="project-link" href="https://codehashira28.github.io/weather-dashboard/">Weather Dashboard</a>
-                    <a href="https://github.com/codehashira28/weather-dashboard">
-                        <img className="github-link" src={github} />
-                    </a>
-                </div>
-                <div className="project">
-                    <img className="project-img" src={techblog} />
-                    <a className="project-link" href="https://tech-blog-app-20230806-f48fb5f8b41d.herokuapp.com/">Tech Blog</a>
-                    <a href="https://github.com/codehashira28/tech-blog-app">
-                        <img className="github-link" src={github} />
-                    </a>
-                </div>
-                <div className="project">
-                    <img className="project-img" src={notetaker} />
-                    <a className="project-link" href="https://pacific-river-80544-d245515ec8bd.herokuapp.com/">Note Taker</a>
-                    <a href="https://github.com/codehashira28/note-taker">
-                        <img className="github-link" src={github} />
-                    </a>
-                </div>
-                <div className="project">
-                    <img className="project-img" src={jate} />
-                    <a className="project-link" href="https://morning-refuge-10692-76df8b5397e0.herokuapp.com/">Text Editor</a>
-                    <a href="https://github.com/codehashira28/pwa-text-editor">
-                        <img className="github-link" src={github} />
-                    </a>
-                </div>
+                <Project projectTitle="Granular Garden" image={granulargarden} appurl="https://frozen-tor-00446-d883d20dd32e.herokuapp.com/" githuburl="https://github.com/codehashira28/granular-garden"/>
+                <Project projectTitle="Poke Info Generator" image={pokeinfogenerator} appurl="https://codehashira28.github.io/poke-info-generator/" githuburl="https://github.com/codehashira28/poke-info-generator"/>
+                <Project projectTitle="Weather Dashboard" image={weatherdashboard} appurl="https://codehashira28.github.io/weather-dashboard/" githuburl="https://github.com/codehashira28/weather-dashboard"/>
+                <Project projectTitle="Tech Blog" image={techblog} appurl="https://tech-blog-app-20230806-f48fb5f8b41d.herokuapp.com/" githuburl="https://github.com/codehashira28/tech-blog-app"/>
+                <Project projectTitle="Note Taker" image={notetaker} appurl="https://pacific-river-80544-d245515ec8bd.herokuapp.com/" githuburl="https://github.com/codehashira28/note-taker"/>
+                <Project projectTitle="Text Editor" image={jate} appurl="https://morning-refuge-10692-76df8b5397e0.herokuapp.com/" githuburl="https://github.com/codehashira28/pwa-text-editor"/>
             </div>
         </div>
     )
