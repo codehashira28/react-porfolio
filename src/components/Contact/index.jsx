b34c383da6c0fbf6dfe5c61912b59ba53b9e8b75
codehashira28 <123044813+codehashira28@users.noreply.github.com>
2023-09-13 23:22:36 -0500

    added logic to check for valid email input

--- src/components/Contact/index.jsx
+++ src/components/Contact/index.jsx
@@ -1,6 +1,25 @@
 import './styles.css';
 
-function Contact() {
+import { useState } from 'react';
+
+
+function ErrorMessage() {
+    return (
+        <p className='fw-bold mt-3 mb-0'>Your email is invalid</p>
+    )
+}
+
+function validate(setEmailCheck) {
+    if(document.getElementById('email').value != "" && !document.getElementById('email').value.match(/@.+$/)) {
+        setEmailCheck(true)
+    } 
+    else {
+        setEmailCheck(false)
+    }
+}
+
+function Contact() { 
+    const [emailCheck, setEmailCheck] = useState(false);
     return (
         <div className="p-5" id="contact">
             <h2>Contact</h2>
@@ -8,12 +27,12 @@ function Contact() {
                 <label htmlFor="name">Name:</label>
                 <input required type="text" id="name"/>
                 <label htmlFor="email">Email:</label>
-                <input id="email" required type="email" />
+                <input id="email" onBlur={() => validate(setEmailCheck)} required type="email" />
                 <label htmlFor="message">Message:</label>
                 <textarea required rows="6" id="message"></textarea>
+                {emailCheck && <ErrorMessage />}
                 <button id="submit" className="mt-3 px-2 align-self-start" type="submit">Submit</button>
             </form>
-            
         </div>
     )
 }
